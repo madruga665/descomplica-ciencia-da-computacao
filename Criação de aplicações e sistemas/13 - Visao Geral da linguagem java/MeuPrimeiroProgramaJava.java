a38dba7b8889822093ce52a14ad38cb3074dfe63
Luciano Amâncio <madruga665@hotmail.com>
2023-11-23 14:06:08 -0300

    meu primeiro programa java #sqn

---
+++ Criação de aplicações e sistemas/13 - Visao Geral da linguagem java/MeuPrimeiroProgramaJava.java
@@ -0,0 +1,15 @@
+public class MeuPrimeiroProgramaJava {
+  public static void main(String entrada[]) {
+    int inteiro = 36;
+    char caracter = 'M';
+    double real = 1.82;
+    String frase = "Luciano Amâncio";
+    boolean VF = true;
+
+    if (VF == true) {
+      System.out.println("Eu sou o " + frase + " tenho " + inteiro + " anos " + real + "m de altura");
+    }
+
+    System.exit(0);
+  }
+}
