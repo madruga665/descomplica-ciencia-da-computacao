7b05dac815bcaeb80e5a8f70f5547e3a854cd2e3
Luciano Amâncio <madruga665@hotmail.com>
2023-11-23 22:05:40 -0300

    Visão geral da linguagem java

---
+++ Criação de aplicações e sistemas/13 - Visao Geral da linguagem java/MeuSegundoProgramaJava.java
@@ -0,0 +1,13 @@
+public class MeuSegundoProgramaJava {
+  public static void main(String entrada[]) {
+    int numero1, numero2, soma;
+
+    numero1 = Integer.parseInt(entrada[0]);
+    numero2 = Integer.parseInt(entrada[1]); 
+    soma = numero1 + numero2;
+
+    System.out.println(numero1 + " + " + numero2 + " = " + soma);
+
+    System.exit(0);
+  }
+}
---
+++ Criação de aplicações e sistemas/13 - Visao Geral da linguagem java/MeusDados.java
@@ -0,0 +1,16 @@
+public class MeusDados {
+  public static void main(String entrada[]) {
+    String nome = "Luciano Amâncio";
+    String curso = "Ciência da Computação";
+    int idade = 36;
+    char genero = 'M';
+    double peso = 72.5;
+
+    System.out.println(
+      "Meu nome é: " + nome + " tenho " + idade + " anos" + " peso " + peso + "kg " + " meu genero é: "
+      + genero + " e estou cursando " + curso + " na faculdade Descomplica."
+    );
+
+    System.exit(0);
+  }
+}
