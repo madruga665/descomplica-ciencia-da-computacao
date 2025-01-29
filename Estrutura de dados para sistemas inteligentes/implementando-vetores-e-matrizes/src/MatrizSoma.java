903ac1901ba5a6c09b48ce17b3930b9cf176c5a2
madruga665 <madruga665@hotmail.com>
2025-01-28 21:25:27 -0300

    Entendendo vetores e matrizes

--- Estrutura de dados para sistemas inteligentes/implementando-vetores-e-matrizes/src/MatrizSoma.java
+++ Estrutura de dados para sistemas inteligentes/implementando-vetores-e-matrizes/src/MatrizSoma.java
@@ -1,18 +1,18 @@
 import javax.swing.*;
 
 class MatrizSoma {
-  public static void main(String args[]) {
-    int MatSoma[][], a, b, soma = 0;
-    MatSoma = new int[3][3];
+	public static void main(String args[]) {
+		int MatSoma[][], a, b, soma = 0;
+		MatSoma = new int[3][3];
 
-    for (a = 0; a <= 2; a++) {
-      for (b = 0; b <= 2; b++) {
-        MatSoma[a][b] = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor inteiro"));
+		for (a = 0; a <= 2; a++) {
+			for (b = 0; b <= 2; b++) {
+				MatSoma[a][b] = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor inteiro"));
 
-        soma = soma + MatSoma[a][b];
-      }
-    }
-    System.out.println("A soma dos valores digitados é: " + soma);
-    System.exit(0);
-  }
+				soma = soma + MatSoma[a][b];
+			}
+		}
+		System.out.println("A soma dos valores digitados é: " + soma);
+		System.exit(0);
+	}
 }
